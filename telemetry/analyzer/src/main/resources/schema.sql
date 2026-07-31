25da27b8654e8ef06f84532d77ec23a74e4dc106
Damir Vafin <damirvafinnailevich@gmail.com>
2026-07-31 16:34:10 +0300

    fix: исправить запуск и обработку событий analyzer

--- telemetry/analyzer/src/main/resources/schema.sql
+++ telemetry/analyzer/src/main/resources/schema.sql
@@ -3,39 +3,39 @@ CREATE TABLE IF NOT EXISTS scenarios (
     hub_id VARCHAR NOT NULL,
     name VARCHAR NOT NULL,
     UNIQUE (hub_id, name)
-);
+)^^^
 
 CREATE TABLE IF NOT EXISTS sensors (
     id VARCHAR PRIMARY KEY,
     hub_id VARCHAR NOT NULL
-);
+)^^^
 
 CREATE TABLE IF NOT EXISTS conditions (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     type VARCHAR NOT NULL,
     operation VARCHAR NOT NULL,
     value INTEGER NOT NULL
-);
+)^^^
 
 CREATE TABLE IF NOT EXISTS actions (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     type VARCHAR NOT NULL,
     value INTEGER
-);
+)^^^
 
 CREATE TABLE IF NOT EXISTS scenario_conditions (
     scenario_id BIGINT REFERENCES scenarios(id),
     sensor_id VARCHAR REFERENCES sensors(id),
     condition_id BIGINT REFERENCES conditions(id),
     PRIMARY KEY (scenario_id, sensor_id, condition_id)
-);
+)^^^
 
 CREATE TABLE IF NOT EXISTS scenario_actions (
     scenario_id BIGINT REFERENCES scenarios(id),
     sensor_id VARCHAR REFERENCES sensors(id),
     action_id BIGINT REFERENCES actions(id),
     PRIMARY KEY (scenario_id, sensor_id, action_id)
-);
+)^^^
 
 CREATE OR REPLACE FUNCTION check_hub_id()
 RETURNS TRIGGER
@@ -59,24 +59,24 @@ BEGIN
 
     RETURN NEW;
 END;
-$$;
+$$^^^
 
 DROP TRIGGER IF EXISTS
     tr_bi_scenario_conditions_hub_id_check
-    ON scenario_conditions;
+    ON scenario_conditions^^^
 
 CREATE TRIGGER tr_bi_scenario_conditions_hub_id_check
 BEFORE INSERT OR UPDATE
 ON scenario_conditions
 FOR EACH ROW
-EXECUTE FUNCTION check_hub_id();
+EXECUTE FUNCTION check_hub_id()^^^
 
 DROP TRIGGER IF EXISTS
     tr_bi_scenario_actions_hub_id_check
-    ON scenario_actions;
+    ON scenario_actions^^^
 
 CREATE TRIGGER tr_bi_scenario_actions_hub_id_check
 BEFORE INSERT OR UPDATE
 ON scenario_actions
 FOR EACH ROW
-EXECUTE FUNCTION check_hub_id();
+EXECUTE FUNCTION check_hub_id()^^^
